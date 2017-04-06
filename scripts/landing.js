69f11963b0ffe1dbd5ef98d2afb006a045f3fc53
Merin Joseph <merinsony18@gmail.com>
2017-04-05 22:54:34 -0700

    checkpoint 10 collectionview: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,6 +7,7 @@ var animatePoints = function(points) {
                     points[index].style.msTransform =" scaleX(1) translateY(0)";
                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }; 
+    
      for (var i=0;i<points.length;i++){
        revealPoint(i); 
     }            
@@ -18,7 +19,8 @@ window.onload = function(){
         animatePoints(pointsArray);
     }
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top- window.innerHeight + 200;
     window.addEventListener('scroll',function(event){
         if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
             animatePoints(pointsArray);
