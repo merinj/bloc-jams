39ef3f94ef15413c0d37a3964fcbcf48a22e86ca
Merin Joseph <merinsony18@gmail.com>
2017-04-08 09:21:37 -0700

    Correct code

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,27 @@
 var pointsArray = document.getElementsByClassName('point');
 
+function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        callback(i);
+    }
+}
+
 var animatePoints = function(points) {
+    
      var revealPoint= function(index){
+         console.log('points', points);
+         console.log('index', index);
         points[index].style.opacity = 1;
                     points[index].style.transform ="scaleX(1) translateY(0)";
                     points[index].style.msTransform =" scaleX(1) translateY(0)";
                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }; 
     
-     for (var i=0;i<points.length;i++){
-       revealPoint(i); 
-    }            
+    forEach(pointsArray,revealPoint);
+    
+//     for (var i=0;i<points.length;i++){
+//       revealPoint(i); 
+//    }            
  };
 
 window.onload = function(){
@@ -18,12 +29,13 @@ window.onload = function(){
     if(window.innerHeight >950){
         animatePoints(pointsArray);
     }
+
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     
-    var scrollDistance = sellingPoints.getBoundingClientRect().top- window.innerHeight + 200;
+    var scrollDistance = sellingPoints.getBoundingClientRect().top- window.innerHeight;// + 200;
     window.addEventListener('scroll',function(event){
         if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
             animatePoints(pointsArray);
         }
     });
-       }    
+}    
