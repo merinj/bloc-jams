b2e3718d4987a931e8dbb04867eed3277051fd4b
Merin Joseph <merinsony18@gmail.com>
2017-04-08 12:36:53 -0700

    Assignment 11 completed

--- scripts/album.js
+++ scripts/album.js
@@ -14,6 +14,20 @@ var albumPicasso = {
  };
  
  // Another Example Album
+ var albumTeenageDreams = {
+     title: 'Tenage Dream',
+     artist: 'Katy Perry',
+     label: 'EMI',
+     year: '2010',
+     albumArtUrl: 'http://vignette1.wikia.nocookie.net/katyperry/images/a/a2/TeenageDreamSingle.jpg/revision/latest?cb=20120412150520',
+     songs: [
+         { title: 'Teenage Dream', duration: '3:48' },
+         { title: 'Last Friday Night', duration: '3:51' },
+         { title: 'California Gurls', duration: '3:55'},
+         { title: 'Firework', duration: '3:48' },
+         { title: 'Peacock', duration: '3:51'}
+     ]
+ };
  var albumMarconi = {
      title: 'The Telephone',
      artist: 'Guglielmo Marconi',
@@ -40,13 +54,15 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
+     // #1
+     
  
      // #2
      albumTitle.firstChild.nodeValue = album.title;
@@ -65,4 +81,14 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var names =[albumPicasso,albumTeenageDreams,albumMarconi];
+     var i=1;
+     albumImage.addEventListener("click",function(event){
+     setCurrentAlbum(names[i]);   
+     i++;
+     if(i== names.length){
+         i=0;
+     }
+ });
  };
