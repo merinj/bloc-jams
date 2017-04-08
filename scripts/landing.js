f75aa82c53442423f864f6d4e9be336590e133e2
Merin Joseph <merinsony18@gmail.com>
2017-04-08 10:43:50 -0700

    checkpoint 11 albumView:completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -9,8 +9,6 @@ function forEach(array, callback) {
 var animatePoints = function(points) {
     
      var revealPoint= function(index){
-         console.log('points', points);
-         console.log('index', index);
         points[index].style.opacity = 1;
                     points[index].style.transform ="scaleX(1) translateY(0)";
                     points[index].style.msTransform =" scaleX(1) translateY(0)";
